e0d493d4c96f96608518fac98f77ca9860462433
Hansel Qiu <qiu_hansel@hotmail.com>
2022-04-14 14:45:04 -0700

    changed to lambda function

--- leap_year.py
+++ leap_year.py
@@ -1,17 +1,16 @@
 import unittest
-
-def is_leap_year(year):
-    return (year%4==0 and (year%100!=0 or year%400==0))
+    
+f = lambda x: (x%4==0 and (x%100!=0 or x%400==0))
 
 class TestLeapYear(unittest.TestCase):
     def test_400_leap_year(self):
-        self.assertEqual(True, is_leap_year(400)) 
+        self.assertEqual(True, f(400)) 
 
     def test_300_leap_year(self):
-        self.assertEqual(False, is_leap_year(300)) 
+        self.assertEqual(False, f(300)) 
 
     def test_4_leap_year(self):
-        self.assertEqual(True, is_leap_year(4)) 
+        self.assertEqual(True, f(4)) 
 
     def test_5_leap_year(self):
-        self.assertEqual(False, is_leap_year(5)) 
+        self.assertEqual(False, f(5)) 
