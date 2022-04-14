4356358558043f41df561095ab13ae2b7a96a871
Hansel Qiu <qiu_hansel@hotmail.com>
2022-04-14 14:28:14 -0700

    reduce to 1 line and add tests

--- leap_year.py
+++ leap_year.py
@@ -1,9 +1,17 @@
+import unittest
+
 def is_leap_year(year):
-    if (year%400 == 0):
-        return True
-    if (year%100 == 0 and year%100 != 0):
-        return False
-    if (year%4 == 0 and year%100!=0):
-        return True 
-    if year%4 != 0:
-        return False
+    return (year%4==0 and (year%100!=0 or year%400==0))
+
+class TestLeapYear(unittest.TestCase):
+    def test_400_leap_year(self):
+        self.assertEqual(True, is_leap_year(400)) 
+
+    def test_300_leap_year(self):
+        self.assertEqual(False, is_leap_year(300)) 
+
+    def test_4_leap_year(self):
+        self.assertEqual(True, is_leap_year(4)) 
+
+    def test_5_leap_year(self):
+        self.assertEqual(False, is_leap_year(5)) 
